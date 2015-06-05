53ed7c58b8935241ae355d0771c853fb36e932fc
Michael Windell <mike@outeredgeuk.com>
2015-06-05 16:19:50 +0100

    added more extensions

--- app/code/local/Edge/ProductAttachments/Model/Observer.php
+++ app/code/local/Edge/ProductAttachments/Model/Observer.php
@@ -13,16 +13,16 @@ class Edge_ProductAttachments_Model_Observer
         $response->setTypes($types);
         return $this;
     }
-    
+
     public function saveFileUploadAttribute(Varien_Event_Observer $observer)
     {
         $product = $observer->getEvent()->getProduct();
-        
+
         if (isset($_FILES['product'])){
-            
+
             foreach ($_FILES['product']['name'] as $attribute=>$filename){
                 if ($filename){
-                    
+
                     $_FILES[$attribute] = array(
                         'name'      => $filename,
                         'type'      => $_FILES['product']['type'][$attribute],
@@ -30,33 +30,33 @@ class Edge_ProductAttachments_Model_Observer
                         'error'     => $_FILES['product']['error'][$attribute],
                         'size'      => $_FILES['product']['size'][$attribute],
                     );
-                    
+
                     if ($uploaded = $this->_saveAttachment($attribute)){
                         $product->setData($attribute, $uploaded);
                     }
                 }
             }
         }
-        
+
         return $this;
     }
-    
+
     protected function _saveAttachment($name)
     {
         $path = Mage::getBaseDir('media') . '/productattachment/';
         if (!file_exists(($path))){
             mkdir($path);
         }
-        
+
         if (isset($_FILES['product']['name'][$name]) && (file_exists($_FILES['product']['tmp_name'][$name]))){
 
             try {
                 $uploader = new Varien_File_Uploader($name);
-                $image = $uploader->setAllowedExtensions(array('jpg','jpeg','gif','png'))
+                $image = $uploader->setAllowedExtensions(array('jpg','jpeg','gif','png','pdf','eps','doc','docx','csv','txt','xls','psd'))
                     ->setAllowRenameFiles(true)
                     ->setFilesDispersion(true)
                     ->save($path, $_FILES['product']['name'][$name]);
-                
+
                 return $image['file'];
 
             } catch (Exception $e){
